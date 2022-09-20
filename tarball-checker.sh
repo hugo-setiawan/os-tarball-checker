1e49c7096fcd1310c4377f747a1d09c867ccad99
Hugo Sulaiman Setiawan <hugo.sulaiman@ui.ac.id>
2022-09-20 15:14:56 +0700

    modified:   tarball-checker.sh Port code for os222, since some changes were found in the tarball such as names, and tar contents.

--- tarball-checker.sh
+++ tarball-checker.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-# Copyright (C) 2021 Muhammad Athallah
+# Copyright (C) 2022 Muhammad Athallah, Hugo Sulaiman Setiawan
 
 # This free document is distributed in the hope that it will be
 # useful, but WITHOUT ANY WARRANTY; without even the implied
 # warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
+# REV05 Tue 20 Sep 2022 15:13:00 WIB
 # REV04 Sun 21 Nov 2021 18:29:25 WIB
 # REV03 Sun 21 Nov 2021 16:45:48 WIB
 # REV02 Tue 19 Oct 2021 13:30:00 WIB
@@ -33,20 +34,20 @@ gpg --decrypt /tmp/tarball-grade/$ACCNAME.tar.bz2.txt > /tmp/tarball-grade/$ACCN
 tar -xf /tmp/tarball-grade/$ACCNAME.tar.bz2 -C /tmp/tarball-grade/mygrade
 
 # Download benchmark
-wget https://cbkadal.github.io/$REPO_NAME/SandBox/TARBALL.tar.bz2 -O /tmp/tarball-grade/TARBALL.tar.bz2
-tar -xf /tmp/tarball-grade/TARBALL.tar.bz2 -C /tmp/tarball-grade/benchmark
+wget https://cbkadal.github.io/$REPO_NAME/SandBox/cbkadal.tar.xz -O /tmp/tarball-grade/cbkadal.tar.xz
+tar -xf /tmp/tarball-grade/cbkadal.tar.xz -C /tmp/tarball-grade/benchmark
 
 # Print grades
 echo "### GRADES OF DW$WEEK ###"
 echo "GRADE DETAILS   | MYGRADE          | BENCHMARK"
 echo "-----------------------------------------------------"
-for file in /tmp/tarball-grade/mygrade/DW$WEEK/*
+for file in /tmp/tarball-grade/mygrade/$ACCNAME/DW$WEEK/*
 do
     FILENAME="${file##*/}"
     FIRSTPART=$(printf '%-15s' "$FILENAME")
     MYGRADE=$(cat $file)
     SECONDPART=$(printf '%-16s' "$MYGRADE")
-    BENCHMARK=$(cat /tmp/tarball-grade/benchmark/DW$WEEK/$FILENAME)
+    BENCHMARK=$(cat /tmp/tarball-grade/benchmark/cbkadal/DW$WEEK/$FILENAME)
     THIRDPART=$(printf '%-16s' "$BENCHMARK")
     echo "$FIRSTPART | $SECONDPART | $THIRDPART"
 done
